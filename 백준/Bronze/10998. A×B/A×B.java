fdd2ee120fbf56d9427b2e84ec3d6112548d0228
milllo-dev <milllodev@gmail.com>
2026-04-07 16:21:09 +0900

    [Bronze V] Title: A×B, Time: 112 ms, Memory: 14520 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void solution() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = reader.readLine().split(" ");
+        int a = Integer.parseInt(str[0]);
+        int b = Integer.parseInt(str[1]);
+        System.out.printf("%d", a * b);
+    }
+    
+    public static void main(String[] args) throws IOException {
+        solution();
+    }
+}
