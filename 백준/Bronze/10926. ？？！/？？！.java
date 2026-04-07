75c6ed5ff96517029d94de2ab6799fdf3306861c
milllo-dev <milllodev@gmail.com>
2026-04-07 16:26:33 +0900

    [Bronze V] Title: ??!, Time: 108 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void solution() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String str = reader.readLine();
+        System.out.print(str + "??!");
+    }
+    public static void main(String[] args) throws IOException {
+        solution();
+    }
+}
