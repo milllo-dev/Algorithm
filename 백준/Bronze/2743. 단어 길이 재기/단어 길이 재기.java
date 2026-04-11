138385e84ed2a399092be1f6ea852f52a1b5d856
milllo-dev <milllodev@gmail.com>
2026-04-11 15:09:47 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 104 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void solution() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(reader.readLine().length());
+        reader.close();
+    }
+    
+    public static void main(String[] args) throws IOException {
+        solution();
+    }
+}
