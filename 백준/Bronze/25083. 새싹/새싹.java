e3bf72f93ead603e4b43cb845f5a49414a33bf5c
milllo-dev <milllodev@gmail.com>
2026-04-13 15:44:45 +0900

    [Bronze V] Title: 새싹, Time: 100 ms, Memory: 14024 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String s = """
+                         ,r'"7
+                r`-_   ,'  ,/
+                 \\. ". L_r'
+                   `~\\/
+                      |
+                      |
+                """;
+        System.out.println(s);
+    }
+}
