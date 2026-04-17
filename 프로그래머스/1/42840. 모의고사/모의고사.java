9d43e435e5a8f46c04a7724975212d8b7deb2113
milllo-dev <milllodev@gmail.com>
2026-04-17 09:43:57 +0900

    [level 1] Title: 모의고사, Time: 16.61 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -3,34 +3,36 @@ import java.io.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        int[] one = {1,2,3,4,5};
-        int[] two = {2,1,2,3,2,4,2,5};
-        int[] three = {3,3,1,1,2,2,4,4,5,5};
-        
-        int[] cntArr = new int[3];
-        
-        for(int i = 0; i < answers.length; i++) {
-            if(answers[i] == one[i%5]) {
-                cntArr[0] += 1;
+        int[] one = { 1, 2, 3, 4, 5 };
+        int[] two = { 2, 1, 2, 3, 2, 4, 2, 5 };
+        int[] three = { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 };
+
+        int[] scores = new int[3];
+
+        ArrayList<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < answers.length; i++) {
+            if (answers[i] == one[i % one.length]) {
+                scores[0]++;
             }
-            if(answers[i] == two[i%8]) {
-                cntArr[1] += 1;
+            if (answers[i] == two[i % two.length]) {
+                scores[1]++;
             }
-            if(answers[i] == three[i%10]) {
-                cntArr[2] += 1;
-            }
-        }
-        
-        int max = Arrays.stream(cntArr).max().getAsInt();
-        
-        ArrayList<Integer> answer = new ArrayList<>();
-        
-        for(int i = 0; i < cntArr.length; i++) {
-            if(cntArr[i] == max) {
-                answer.add(i+1);
+            if (answers[i] == three[i % three.length]) {
+                scores[2]++;
             }
         }
+
+        int max = Arrays.stream(scores).max().getAsInt();
+
+        for (int i = 0; i < scores.length; i++) {
+            if (scores[i] == max) {
+                result.add(i + 1);
+            }
+        }
+
+        System.out.println("one : " + scores[0] + "/ two : " + scores[1] + " / three : " + scores[2]);
         
-        return answer.stream().sorted().mapToInt(Integer::intValue).toArray();
+        return result.stream().mapToInt(Integer::intValue).toArray();
     }
 }
