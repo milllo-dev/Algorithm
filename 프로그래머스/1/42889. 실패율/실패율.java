7171a02f958ad3645fdbd623b0c260ba9bfdc904
milllo-dev <milllodev@gmail.com>
2026-04-20 18:31:32 +0900

    [level 1] Title: 실패율, Time: 15.72 ms, Memory: 103 MB -BaekjoonHub

---
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+import java.util.*;
+
+class Solution {
+    public int[] solution(int N, int[] stages) {int[] chCounts = new int[N + 2];
+
+        for (int stage : stages) {
+            chCounts[stage]++;
+        }
+
+        double total = stages.length;
+
+        // 실패율
+        Map<Integer, Double> fail = new HashMap<>();
+
+        for (int i = 1; i <= N; i++) {
+            if (chCounts[i] == 0) {
+                fail.put(i, 0.);
+            } else {
+                fail.put(i, chCounts[i] / total);
+                total -= chCounts[i];
+            }
+        }
+
+        System.out.println(fail);
+
+        // compare
+        // (리턴값) a < b -> 음수
+        // a == b -> 0
+        // a > b -> 양수
+        return fail.entrySet().stream()
+                .sorted((o1, o2) -> o1.getValue().equals(o2.getValue()) ? Integer.compare(o1.getKey(), o2.getKey())
+                        : Double.compare(o2.getValue(), o1.getValue()))
+                .mapToInt(HashMap.Entry::getKey).toArray();
+    }
+}
